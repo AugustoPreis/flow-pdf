4cb828c70d90e641cf1da059a33da9f8059d0a49
Augusto Tomasi <augustopreisthomasi@gmail.com>
2026-02-24 12:55:28 -0300

    chore: support for alias import in tests

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,11 @@ module.exports = {
   collectCoverage: true,
   passWithNoTests: true,
   testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
+
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+
   collectCoverageFrom: [
     'src/**/*.ts',
     '!src/**/*.d.ts',
